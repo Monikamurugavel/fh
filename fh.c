b5fc6c386ad374c8099b715ab2075f5ccfcda6d3
Monikamurugavel <92583916+Monikamurugavel@users.noreply.github.com>
2021-10-31 06:58:38 -0700

    uploaded file handling
    this file print reverse order of the file content and copy form one to another file

---
+++ fh.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+      int i,pos,ch;
+      FILE *fp,*fq;
+      fp=fopen("source.txt","r");
+      fq=fopen("backup.txt","w");   
+      if(fp==NULL||fq==NULL)
+            printf("File does not exist..");
+      else
+            while((ch=fgetc(fp))!=EOF)
+            {
+                  fputc(ch,fq);
+            }
+      printf("File copied.....");
+      fseek(fp,0,SEEK_END);
+      pos=ftell(fp);
+      //printf("Current postion is %d\n",pos);
+      i=0;
+      while(i<pos)
+       {
+            i++;
+            fseek(fp,-i,SEEK_END);
+            //printf("%c",fgetc(fp));
+            ch=fgetc(fp);
+            printf("%c",ch);
+      }
+      return 0;
+}
